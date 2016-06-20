aa988579a26eee36b3ad874f663c1384829b3aa1
Etruscian <sanswart@gmail.com>
2016-06-20 15:18:42 +0200

    quick commit nothing fancy

--- Pre-recorded Gait/gait_playerbio.m
+++ Pre-recorded Gait/gait_playerbio.m
@@ -11,6 +11,526 @@ bio(1).size=[];
 bio(1).isStruct=false;
 bio(getlenBIO) = bio(1);
 
+bio(2).blkName='Instrument Panel LHFE/velocityOverrideFlag';
+bio(2).sigName='';
+bio(2).portIdx=0;
+bio(2).dim=[1,1];
+bio(2).sigWidth=1;
+bio(2).sigAddress='&gait_player_B.velocityOverrideFlag';
+bio(2).ndims=2;
+bio(2).size=[];
+bio(2).isStruct=false;
+
+bio(3).blkName='Instrument Panel LHFE/phaseoffset2';
+bio(3).sigName='';
+bio(3).portIdx=0;
+bio(3).dim=[1,1];
+bio(3).sigWidth=1;
+bio(3).sigAddress='&gait_player_B.phaseoffset2';
+bio(3).ndims=2;
+bio(3).size=[];
+bio(3).isStruct=false;
+
+bio(4).blkName='Instrument Panel LHFE/Gain';
+bio(4).sigName='';
+bio(4).portIdx=0;
+bio(4).dim=[1,1];
+bio(4).sigWidth=1;
+bio(4).sigAddress='&gait_player_B.Gain';
+bio(4).ndims=2;
+bio(4).size=[];
+bio(4).isStruct=false;
+
+bio(5).blkName='Instrument Panel LHFE/Gain1';
+bio(5).sigName='';
+bio(5).portIdx=0;
+bio(5).dim=[1,1];
+bio(5).sigWidth=1;
+bio(5).sigAddress='&gait_player_B.Gain1';
+bio(5).ndims=2;
+bio(5).size=[];
+bio(5).isStruct=false;
+
+bio(6).blkName='Instrument Panel LHFE/Gain2';
+bio(6).sigName='';
+bio(6).portIdx=0;
+bio(6).dim=[1,1];
+bio(6).sigWidth=1;
+bio(6).sigAddress='&gait_player_B.Gain2';
+bio(6).ndims=2;
+bio(6).size=[];
+bio(6).isStruct=false;
+
+bio(7).blkName='Instrument Panel LHFE/Sum1';
+bio(7).sigName='';
+bio(7).portIdx=0;
+bio(7).dim=[1,1];
+bio(7).sigWidth=1;
+bio(7).sigAddress='&gait_player_B.Sum1';
+bio(7).ndims=2;
+bio(7).size=[];
+bio(7).isStruct=false;
+
+bio(8).blkName='Instrument Panel LKFE/velocityOverrideFlag';
+bio(8).sigName='';
+bio(8).portIdx=0;
+bio(8).dim=[1,1];
+bio(8).sigWidth=1;
+bio(8).sigAddress='&gait_player_B.velocityOverrideFlag_c';
+bio(8).ndims=2;
+bio(8).size=[];
+bio(8).isStruct=false;
+
+bio(9).blkName='Instrument Panel LKFE/phaseoffset1';
+bio(9).sigName='';
+bio(9).portIdx=0;
+bio(9).dim=[1,1];
+bio(9).sigWidth=1;
+bio(9).sigAddress='&gait_player_B.phaseoffset1';
+bio(9).ndims=2;
+bio(9).size=[];
+bio(9).isStruct=false;
+
+bio(10).blkName='Instrument Panel LKFE/Gain3';
+bio(10).sigName='';
+bio(10).portIdx=0;
+bio(10).dim=[1,1];
+bio(10).sigWidth=1;
+bio(10).sigAddress='&gait_player_B.Gain3';
+bio(10).ndims=2;
+bio(10).size=[];
+bio(10).isStruct=false;
+
+bio(11).blkName='Instrument Panel LKFE/Gain4';
+bio(11).sigName='';
+bio(11).portIdx=0;
+bio(11).dim=[1,1];
+bio(11).sigWidth=1;
+bio(11).sigAddress='&gait_player_B.Gain4';
+bio(11).ndims=2;
+bio(11).size=[];
+bio(11).isStruct=false;
+
+bio(12).blkName='Instrument Panel LKFE/Gain5';
+bio(12).sigName='';
+bio(12).portIdx=0;
+bio(12).dim=[1,1];
+bio(12).sigWidth=1;
+bio(12).sigAddress='&gait_player_B.Gain5';
+bio(12).ndims=2;
+bio(12).size=[];
+bio(12).isStruct=false;
+
+bio(13).blkName='Instrument Panel LKFE/Sum2';
+bio(13).sigName='';
+bio(13).portIdx=0;
+bio(13).dim=[1,1];
+bio(13).sigWidth=1;
+bio(13).sigAddress='&gait_player_B.Sum2';
+bio(13).ndims=2;
+bio(13).size=[];
+bio(13).isStruct=false;
+
+bio(14).blkName='Instrument Panel RHFE/velocityOverrideFlag';
+bio(14).sigName='';
+bio(14).portIdx=0;
+bio(14).dim=[1,1];
+bio(14).sigWidth=1;
+bio(14).sigAddress='&gait_player_B.velocityOverrideFlag_k';
+bio(14).ndims=2;
+bio(14).size=[];
+bio(14).isStruct=false;
+
+bio(15).blkName='Instrument Panel RHFE/phaseoffset2';
+bio(15).sigName='';
+bio(15).portIdx=0;
+bio(15).dim=[1,1];
+bio(15).sigWidth=1;
+bio(15).sigAddress='&gait_player_B.phaseoffset2_c';
+bio(15).ndims=2;
+bio(15).size=[];
+bio(15).isStruct=false;
+
+bio(16).blkName='Instrument Panel RHFE/Gain';
+bio(16).sigName='';
+bio(16).portIdx=0;
+bio(16).dim=[1,1];
+bio(16).sigWidth=1;
+bio(16).sigAddress='&gait_player_B.Gain_d';
+bio(16).ndims=2;
+bio(16).size=[];
+bio(16).isStruct=false;
+
+bio(17).blkName='Instrument Panel RHFE/Gain1';
+bio(17).sigName='';
+bio(17).portIdx=0;
+bio(17).dim=[1,1];
+bio(17).sigWidth=1;
+bio(17).sigAddress='&gait_player_B.Gain1_m';
+bio(17).ndims=2;
+bio(17).size=[];
+bio(17).isStruct=false;
+
+bio(18).blkName='Instrument Panel RHFE/Gain2';
+bio(18).sigName='';
+bio(18).portIdx=0;
+bio(18).dim=[1,1];
+bio(18).sigWidth=1;
+bio(18).sigAddress='&gait_player_B.Gain2_k';
+bio(18).ndims=2;
+bio(18).size=[];
+bio(18).isStruct=false;
+
+bio(19).blkName='Instrument Panel RHFE/Sum1';
+bio(19).sigName='';
+bio(19).portIdx=0;
+bio(19).dim=[1,1];
+bio(19).sigWidth=1;
+bio(19).sigAddress='&gait_player_B.Sum1_k';
+bio(19).ndims=2;
+bio(19).size=[];
+bio(19).isStruct=false;
+
+bio(20).blkName='Instrument Panel RKFE/velocityOverrideFlag';
+bio(20).sigName='';
+bio(20).portIdx=0;
+bio(20).dim=[1,1];
+bio(20).sigWidth=1;
+bio(20).sigAddress='&gait_player_B.velocityOverrideFlag_l';
+bio(20).ndims=2;
+bio(20).size=[];
+bio(20).isStruct=false;
+
+bio(21).blkName='Instrument Panel RKFE/phaseoffset1';
+bio(21).sigName='';
+bio(21).portIdx=0;
+bio(21).dim=[1,1];
+bio(21).sigWidth=1;
+bio(21).sigAddress='&gait_player_B.phaseoffset1_i';
+bio(21).ndims=2;
+bio(21).size=[];
+bio(21).isStruct=false;
+
+bio(22).blkName='Instrument Panel RKFE/Gain3';
+bio(22).sigName='';
+bio(22).portIdx=0;
+bio(22).dim=[1,1];
+bio(22).sigWidth=1;
+bio(22).sigAddress='&gait_player_B.Gain3_g';
+bio(22).ndims=2;
+bio(22).size=[];
+bio(22).isStruct=false;
+
+bio(23).blkName='Instrument Panel RKFE/Gain4';
+bio(23).sigName='';
+bio(23).portIdx=0;
+bio(23).dim=[1,1];
+bio(23).sigWidth=1;
+bio(23).sigAddress='&gait_player_B.Gain4_j';
+bio(23).ndims=2;
+bio(23).size=[];
+bio(23).isStruct=false;
+
+bio(24).blkName='Instrument Panel RKFE/Gain5';
+bio(24).sigName='';
+bio(24).portIdx=0;
+bio(24).dim=[1,1];
+bio(24).sigWidth=1;
+bio(24).sigAddress='&gait_player_B.Gain5_c';
+bio(24).ndims=2;
+bio(24).size=[];
+bio(24).isStruct=false;
+
+bio(25).blkName='Instrument Panel RKFE/Sum2';
+bio(25).sigName='';
+bio(25).portIdx=0;
+bio(25).dim=[1,1];
+bio(25).sigWidth=1;
+bio(25).sigAddress='&gait_player_B.Sum2_e';
+bio(25).ndims=2;
+bio(25).size=[];
+bio(25).isStruct=false;
+
+bio(26).blkName='LHFE/EtherCAT PDO Receive';
+bio(26).sigName='LHFE jointAngle';
+bio(26).portIdx=0;
+bio(26).dim=[1,1];
+bio(26).sigWidth=1;
+bio(26).sigAddress='&gait_player_B.LHFEjointAngle';
+bio(26).ndims=2;
+bio(26).size=[];
+bio(26).isStruct=false;
+
+bio(27).blkName='LHFE/EtherCAT PDO Receive1';
+bio(27).sigName='LHFE motorAngle';
+bio(27).portIdx=0;
+bio(27).dim=[1,1];
+bio(27).sigWidth=1;
+bio(27).sigAddress='&gait_player_B.LHFEmotorAngle';
+bio(27).ndims=2;
+bio(27).size=[];
+bio(27).isStruct=false;
+
+bio(28).blkName='LHFE/EtherCAT PDO Receive2';
+bio(28).sigName='LHFE motorVelocity';
+bio(28).portIdx=0;
+bio(28).dim=[1,1];
+bio(28).sigWidth=1;
+bio(28).sigAddress='&gait_player_B.LHFEmotorVelocity';
+bio(28).ndims=2;
+bio(28).size=[];
+bio(28).isStruct=false;
+
+bio(29).blkName='LHFE Gait Generation/LHFE home and walk';
+bio(29).sigName='';
+bio(29).portIdx=0;
+bio(29).dim=[1,1];
+bio(29).sigWidth=1;
+bio(29).sigAddress='&gait_player_B.y_b';
+bio(29).ndims=2;
+bio(29).size=[];
+bio(29).isStruct=false;
+
+bio(30).blkName='LHFE Gait Generation/Clock';
+bio(30).sigName='';
+bio(30).portIdx=0;
+bio(30).dim=[1,1];
+bio(30).sigWidth=1;
+bio(30).sigAddress='&gait_player_B.Clock';
+bio(30).ndims=2;
+bio(30).size=[];
+bio(30).isStruct=false;
+
+bio(31).blkName='LHFE Gait Generation/Data Type Conversion';
+bio(31).sigName='';
+bio(31).portIdx=0;
+bio(31).dim=[1,1];
+bio(31).sigWidth=1;
+bio(31).sigAddress='&gait_player_B.DataTypeConversion';
+bio(31).ndims=2;
+bio(31).size=[];
+bio(31).isStruct=false;
+
+bio(32).blkName='LHFE Gait Generation/Data Type Conversion1';
+bio(32).sigName='';
+bio(32).portIdx=0;
+bio(32).dim=[1,1];
+bio(32).sigWidth=1;
+bio(32).sigAddress='&gait_player_B.DataTypeConversion1';
+bio(32).ndims=2;
+bio(32).size=[];
+bio(32).isStruct=false;
+
+bio(33).blkName='LKFE/EtherCAT PDO Receive';
+bio(33).sigName='LKFE jointAngle';
+bio(33).portIdx=0;
+bio(33).dim=[1,1];
+bio(33).sigWidth=1;
+bio(33).sigAddress='&gait_player_B.LKFEjointAngle';
+bio(33).ndims=2;
+bio(33).size=[];
+bio(33).isStruct=false;
+
+bio(34).blkName='LKFE/EtherCAT PDO Receive1';
+bio(34).sigName='LKFE motorAngle';
+bio(34).portIdx=0;
+bio(34).dim=[1,1];
+bio(34).sigWidth=1;
+bio(34).sigAddress='&gait_player_B.LKFEmotorAngle';
+bio(34).ndims=2;
+bio(34).size=[];
+bio(34).isStruct=false;
+
+bio(35).blkName='LKFE/EtherCAT PDO Receive2';
+bio(35).sigName='LKFE motorVelocity';
+bio(35).portIdx=0;
+bio(35).dim=[1,1];
+bio(35).sigWidth=1;
+bio(35).sigAddress='&gait_player_B.LKFEmotorVelocity';
+bio(35).ndims=2;
+bio(35).size=[];
+bio(35).isStruct=false;
+
+bio(36).blkName='LKFE Gait Generation /LKFE home and walk';
+bio(36).sigName='';
+bio(36).portIdx=0;
+bio(36).dim=[1,1];
+bio(36).sigWidth=1;
+bio(36).sigAddress='&gait_player_B.y_j';
+bio(36).ndims=2;
+bio(36).size=[];
+bio(36).isStruct=false;
+
+bio(37).blkName='LKFE Gait Generation /RKFE Clock';
+bio(37).sigName='';
+bio(37).portIdx=0;
+bio(37).dim=[1,1];
+bio(37).sigWidth=1;
+bio(37).sigAddress='&gait_player_B.RKFEClock';
+bio(37).ndims=2;
+bio(37).size=[];
+bio(37).isStruct=false;
+
+bio(38).blkName='LKFE Gait Generation /Data Type Conversion';
+bio(38).sigName='';
+bio(38).portIdx=0;
+bio(38).dim=[1,1];
+bio(38).sigWidth=1;
+bio(38).sigAddress='&gait_player_B.DataTypeConversion_m';
+bio(38).ndims=2;
+bio(38).size=[];
+bio(38).isStruct=false;
+
+bio(39).blkName='LKFE Gait Generation /Data Type Conversion1';
+bio(39).sigName='';
+bio(39).portIdx=0;
+bio(39).dim=[1,1];
+bio(39).sigWidth=1;
+bio(39).sigAddress='&gait_player_B.DataTypeConversion1_b';
+bio(39).ndims=2;
+bio(39).size=[];
+bio(39).isStruct=false;
+
+bio(40).blkName='RHFE/EtherCAT PDO Receive';
+bio(40).sigName='RHFE jointAngle';
+bio(40).portIdx=0;
+bio(40).dim=[1,1];
+bio(40).sigWidth=1;
+bio(40).sigAddress='&gait_player_B.RHFEjointAngle';
+bio(40).ndims=2;
+bio(40).size=[];
+bio(40).isStruct=false;
+
+bio(41).blkName='RHFE/EtherCAT PDO Receive1';
+bio(41).sigName='RHFE motorAngle';
+bio(41).portIdx=0;
+bio(41).dim=[1,1];
+bio(41).sigWidth=1;
+bio(41).sigAddress='&gait_player_B.RHFEmotorAngle';
+bio(41).ndims=2;
+bio(41).size=[];
+bio(41).isStruct=false;
+
+bio(42).blkName='RHFE/EtherCAT PDO Receive2';
+bio(42).sigName='RHFE motorVelocity';
+bio(42).portIdx=0;
+bio(42).dim=[1,1];
+bio(42).sigWidth=1;
+bio(42).sigAddress='&gait_player_B.RHFEmotorVelocity';
+bio(42).ndims=2;
+bio(42).size=[];
+bio(42).isStruct=false;
+
+bio(43).blkName='RHFE Gait Generation/RHFE home and walk';
+bio(43).sigName='';
+bio(43).portIdx=0;
+bio(43).dim=[1,1];
+bio(43).sigWidth=1;
+bio(43).sigAddress='&gait_player_B.y_a';
+bio(43).ndims=2;
+bio(43).size=[];
+bio(43).isStruct=false;
+
+bio(44).blkName='RHFE Gait Generation/Clock';
+bio(44).sigName='';
+bio(44).portIdx=0;
+bio(44).dim=[1,1];
+bio(44).sigWidth=1;
+bio(44).sigAddress='&gait_player_B.Clock_p';
+bio(44).ndims=2;
+bio(44).size=[];
+bio(44).isStruct=false;
+
+bio(45).blkName='RHFE Gait Generation/Data Type Conversion';
+bio(45).sigName='';
+bio(45).portIdx=0;
+bio(45).dim=[1,1];
+bio(45).sigWidth=1;
+bio(45).sigAddress='&gait_player_B.DataTypeConversion_d';
+bio(45).ndims=2;
+bio(45).size=[];
+bio(45).isStruct=false;
+
+bio(46).blkName='RHFE Gait Generation/Data Type Conversion1';
+bio(46).sigName='';
+bio(46).portIdx=0;
+bio(46).dim=[1,1];
+bio(46).sigWidth=1;
+bio(46).sigAddress='&gait_player_B.DataTypeConversion1_c';
+bio(46).ndims=2;
+bio(46).size=[];
+bio(46).isStruct=false;
+
+bio(47).blkName='RKFE/EtherCAT PDO Receive';
+bio(47).sigName='RKFE jointAngle';
+bio(47).portIdx=0;
+bio(47).dim=[1,1];
+bio(47).sigWidth=1;
+bio(47).sigAddress='&gait_player_B.RKFEjointAngle';
+bio(47).ndims=2;
+bio(47).size=[];
+bio(47).isStruct=false;
+
+bio(48).blkName='RKFE/EtherCAT PDO Receive1';
+bio(48).sigName='RKFE motorAngle';
+bio(48).portIdx=0;
+bio(48).dim=[1,1];
+bio(48).sigWidth=1;
+bio(48).sigAddress='&gait_player_B.RKFEmotorAngle';
+bio(48).ndims=2;
+bio(48).size=[];
+bio(48).isStruct=false;
+
+bio(49).blkName='RKFE/EtherCAT PDO Receive2';
+bio(49).sigName='RKFE motorVelocity';
+bio(49).portIdx=0;
+bio(49).dim=[1,1];
+bio(49).sigWidth=1;
+bio(49).sigAddress='&gait_player_B.RKFEmotorVelocity';
+bio(49).ndims=2;
+bio(49).size=[];
+bio(49).isStruct=false;
+
+bio(50).blkName='RKFE Gait Generation/RKFE home and walk';
+bio(50).sigName='';
+bio(50).portIdx=0;
+bio(50).dim=[1,1];
+bio(50).sigWidth=1;
+bio(50).sigAddress='&gait_player_B.y';
+bio(50).ndims=2;
+bio(50).size=[];
+bio(50).isStruct=false;
+
+bio(51).blkName='RKFE Gait Generation/RKFE Clock';
+bio(51).sigName='';
+bio(51).portIdx=0;
+bio(51).dim=[1,1];
+bio(51).sigWidth=1;
+bio(51).sigAddress='&gait_player_B.RKFEClock_d';
+bio(51).ndims=2;
+bio(51).size=[];
+bio(51).isStruct=false;
+
+bio(52).blkName='RKFE Gait Generation/Data Type Conversion';
+bio(52).sigName='';
+bio(52).portIdx=0;
+bio(52).dim=[1,1];
+bio(52).sigWidth=1;
+bio(52).sigAddress='&gait_player_B.DataTypeConversion_c';
+bio(52).ndims=2;
+bio(52).size=[];
+bio(52).isStruct=false;
+
+bio(53).blkName='RKFE Gait Generation/Data Type Conversion1';
+bio(53).sigName='';
+bio(53).portIdx=0;
+bio(53).dim=[1,1];
+bio(53).sigWidth=1;
+bio(53).sigAddress='&gait_player_B.DataTypeConversion1_k';
+bio(53).ndims=2;
+bio(53).size=[];
+bio(53).isStruct=false;
+
 function len = getlenBIO
-len = 1;
+len = 53;
 
