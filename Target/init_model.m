49e195cf6c7104b8af45093c85d7789d28e6822e
Ortix <nicktsutsunava@gmail.com>
2016-07-09 16:50:41 +0200

    codegen file control

---
+++ Target/init_model.m
@@ -0,0 +1,5 @@
+%%% DO NOT LET MATLAB/SIMULINK CHANGE DIRECTORIES IN THIS SCRIPT
+%%% USE THE MODEL PROPERTIES CALLBACKS FOR THAT
+
+% Include libraries to path
+addpath(genpath(fullfile(fileparts(pwd),'libraries')));
