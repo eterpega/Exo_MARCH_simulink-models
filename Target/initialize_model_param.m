1e30f77ff5f246426b008d98502f2f3ee3670ae6
Etruscian <sanswart@gmail.com>
2016-07-13 12:35:55 +0200

    updated model and added script to transfer data

--- Target/initialize_model_param.m
+++ Target/initialize_model_param.m
@@ -4,3 +4,6 @@ par.pelvis_width = 135.213424/1000;
 par.pelvis_depth = 207.340833/1000;
 par.upper_length = 467.447184/1000;
 par.lower_length = 567.470264/1000;
+
+par.joint_limit_max = [0 0 0 0 0 0];
+par.joint_limit_min = [0 0 0 0 0 0];
