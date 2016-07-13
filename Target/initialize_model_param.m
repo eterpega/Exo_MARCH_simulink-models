0e48f31d50a5913bf446da2d25e24aeac329a70c
Etruscian <sanswart@gmail.com>
2016-07-13 15:46:55 +0200

    trying to figure out why joint angles are not corr

--- Target/initialize_model_param.m
+++ Target/initialize_model_param.m
@@ -5,5 +5,5 @@ par.pelvis_depth = 207.340833/1000;
 par.upper_length = 467.447184/1000;
 par.lower_length = 567.470264/1000;
 
-par.joint_limit_max = [0 0 0 0 0 0];
-par.joint_limit_min = [0 0 0 0 0 0];
+par.joint_limit_max = [-20 -109 -3 -23 -109 -3]/180*pi;
+par.joint_limit_min = [23 21 121 20 21 121]/180*pi;
