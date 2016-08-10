922c460b63b5d0b4e42e6b98453f86223424fb4a
Rafael Argiro <rafael.argiro@gmail.com>
2016-08-10 14:38:25 +0200

    working Check input device state flow

--- Target State Machine/init_StateMachine_test.m
+++ Target State Machine/init_StateMachine_test.m
@@ -1,7 +1,7 @@
 set_param('StateMachine_test/Right Trigger', 'Value', '0')
 set_param('StateMachine_test/Right Safety', 'Value', '0')
 set_param('StateMachine_test/Right Scroll Click', 'Value', '0')
-set_param('StateMachine_test/Right Scroll Mode', 'Value', '0')
+set_param('StateMachine_test/Right Scroll Mode', 'Value', '1')
 set_param('StateMachine_test/Left Trigger', 'Value', '0')
 set_param('StateMachine_test/Left Safety', 'Value', '0')
 set_param('StateMachine_test/Both Triggers', 'Value', '0')
