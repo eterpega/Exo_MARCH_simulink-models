4fc93513a3b45b96d82a40095d82d5a0298ee00f
Rafael Argiro <rafael.argiro@gmail.com>
2016-08-10 15:17:20 +0200

    Working transitions between startup, zero torque, sitting and standing.

--- Target State Machine/init_StateMachine_test.m
+++ Target State Machine/init_StateMachine_test.m
@@ -1,8 +1,13 @@
 set_param('StateMachine_test/Right Trigger', 'Value', '0')
+set_param('StateMachine_test/Left Trigger', 'Value', '0')
+set_param('StateMachine_test/Both Triggers', 'Value', '0')
+
 set_param('StateMachine_test/Right Safety', 'Value', '0')
+set_param('StateMachine_test/Left Safety', 'Value', '0')
+set_param('StateMachine_test/Both Safety', 'Value', '0')
+
 set_param('StateMachine_test/Right Scroll Click', 'Value', '0')
 set_param('StateMachine_test/Right Scroll Mode', 'Value', '1')
-set_param('StateMachine_test/Left Trigger', 'Value', '0')
-set_param('StateMachine_test/Left Safety', 'Value', '0')
-set_param('StateMachine_test/Both Triggers', 'Value', '0')
-set_param('StateMachine_test/Both Safety', 'Value', '0')
+
+set_param('StateMachine_test/doneSitting', 'Value', '0')
+set_param('StateMachine_test/doneStanding', 'Value', '0')
