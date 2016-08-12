7693dbd496780f55e5b68f77952765c707bad017
Ortix <nicktsutsunava@gmail.com>
2016-08-12 10:23:48 +0200

    Git is being annoying

--- Target/test.m
+++ Target/test.m
@@ -1,64 +1,35 @@
-udp = dsp.UDPSender()
+% Initialize
+clear all
+close all
+clc
+udp = dsp.UDPSender();
 s = splineController();
 r = robotics();
 base = 1;
-torso_delta = [0 0.1 0];
+q = [0 -1.92 1.8 0 -1.92 1.8];
 
-r.base = base;
-r.torso_offset = [0 0.03 0];
-%% Weight Shift
-if(base == 1)
-    torso_delta(2) = -abs(torso_delta(2));
-else
-    torso_delta(2) = abs(torso_delta(2));
-end
 
-torsoTrajectory = s.getWeightShift([0 r.torso(2) 0], [0 0 0]);
+r.q = q;
+% Calculate Trajectories
+samples = 5000;
+q_hfe = linspace(q(2),0,4000);
+q_kfe = linspace(q(3),0,samples);
 
-for i = 1:length(torsoTrajectory)
-    r.weightShift(torsoTrajectory(i,:));
-    r.inverseKinematics();
+q_t = zeros(samples,6);
+
+% Visualize
+wait = samples-length(q_hfe);
+for i = 1:samples
     
-    q = r.q;
+    q(3) = q_kfe(i);
+    q(6) = q_kfe(i);
+    if(i > wait)
+        q(2) = q_hfe(i-wait);
+        q(5) = q_hfe(i-wait);
+    end
+    
     step(udp,q);
-    pause(1/2000);
-end
-r.refresh();
-return
-%% Step sequence
-s.setStepHeight(0.1).setStepLength(0.3);
-
-trajectory = s.method('circle').setVelocity(0.5).getHalfStep();
-trajectory = r.transformSpline(trajectory);
-
-% for j = 1:length(trajectory)
-%     r.point = [trajectory(1,j) trajectory(2,j) trajectory(3,j)];
-%     
-% %     r.torso_offset = [0 0.0000*j 0 ];
-%     
-%     r.step();
-%     r.inverseKinematics();
-%     
-%     q = r.q;
-%     step(udp,q);
-%     pause(1/2000);
-% end
-
-r.base = 1;
-trajectory = s.method('circle').setVelocity(0.5).getFullStep();
-trajectory = r.transformSpline(trajectory);
-
-for j = 1:length(trajectory)
-    r.point = [trajectory(1,j) trajectory(2,j) trajectory(3,j)];
-    
-    r.step();
-    r.inverseKinematics();
-    
-    r.refresh();
-    
-    q = r.q;
-    step(udp,q);
-    pause(1/2000);
+    pause(1/500);
 end
 
 
