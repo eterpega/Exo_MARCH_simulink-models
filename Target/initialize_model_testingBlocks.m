410d09d8080210a6af2a2e2f6d3d9fb24f31ac59
Rafael Argiro <rafael.argiro@gmail.com>
2016-08-13 13:21:46 +0200

    Changed locations of doneVaraibles in the model.

--- Target/initialize_model_testingBlocks.m
+++ Target/initialize_model_testingBlocks.m
@@ -16,7 +16,7 @@ set_param('MARCH_v1_5/Input Device/Right Scroll Click', 'Value', '0')
 set_param('MARCH_v1_5/Input Device/Right Scroll Mode', 'Value', '1')
 
 % Controller feedback: done variables
-set_param('MARCH_v1_5/State Machine/doneSitting', 'Value', '0')
-set_param('MARCH_v1_5/State Machine/doneStanding', 'Value', '0')
-set_param('MARCH_v1_5/State Machine/doneStepConst', 'Value', '0')
-set_param('MARCH_v1_5/State Machine/doneStepPeak', 'Value', '0')
+set_param('MARCH_v1_5/Input Device/doneSitting', 'Value', '0')
+set_param('MARCH_v1_5/Input Device/doneStanding', 'Value', '0')
+set_param('MARCH_v1_5/Input Device/doneStepConst', 'Value', '0')
+set_param('MARCH_v1_5/Input Device/doneStepPeak', 'Value', '0')
