42187508199709de65e4e963f165a8f657bcb270
Rafael Argiro <rafael.argiro@gmail.com>
2016-08-13 12:58:28 +0200

    Cleaned up the model and ran first test.
    All transitions are working (did manual input to State Machine).
    Next step: change location for done variables for testing.

--- Target/run.m
+++ Target/run.m
@@ -43,25 +43,25 @@ set_param([model{1} '/Point&Go/EtherCAT Point&Go'],'commented',target)
 set_param([model{1} '/EtherCAT Joint Data In'],'commented',target)
 set_param([model{1} '/EtherCAT Joint Setpoints'],'commented',target)
 
-% Initialize all local simulation/testing blocks to specified value
-if localValue
-    % Input device: Trigger buttons
-    set_param('MARCH_v1_5/Input Device/Right Trigger', 'Value', '0')
-    set_param('MARCH_v1_5/Input Device/Left Trigger', 'Value', '0')
-    set_param('MARCH_v1_5/Input Device/Both Triggers', 'Value', '0')
-    % Input device: Safety buttons
-    set_param('MARCH_v1_5/Input Device/Right Safety', 'Value', '0')
-    set_param('MARCH_v1_5/Input Device/Left Safety', 'Value', '0')
-    set_param('MARCH_v1_5/Input Device/Both Safety', 'Value', '0')
-    % Input device: scroll wheel
-    set_param('MARCH_v1_5/Input Device/Right Scroll Click', 'Value', '0')
-    set_param('MARCH_v1_5/Input Device/Right Scroll Mode', 'Value', '1')
-    % Controller feedback: done variables
-    set_param('MARCH_v1_5/State Machine/doneSitting', 'Value', '0')
-    set_param('MARCH_v1_5/State Machine/doneStanding', 'Value', '0')
-    set_param('MARCH_v1_5/State Machine/doneStepConst', 'Value', '0')
-    set_param('MARCH_v1_5/State Machine/doneStepPeak', 'Value', '0')
-end
+% % Initialize all local simulation/testing blocks to specified value
+% if localValue
+%     % Input device: Trigger buttons
+%     set_param('MARCH_v1_5/Input Device/Right Trigger', 'Value', '0')
+%     set_param('MARCH_v1_5/Input Device/Left Trigger', 'Value', '0')
+%     set_param('MARCH_v1_5/Input Device/Both Triggers', 'Value', '0')
+%     % Input device: Safety buttons
+%     set_param('MARCH_v1_5/Input Device/Right Safety', 'Value', '0')
+%     set_param('MARCH_v1_5/Input Device/Left Safety', 'Value', '0')
+%     set_param('MARCH_v1_5/Input Device/Both Safety', 'Value', '0')
+%     % Input device: scroll wheel
+%     set_param('MARCH_v1_5/Input Device/Right Scroll Click', 'Value', '0')
+%     set_param('MARCH_v1_5/Input Device/Right Scroll Mode', 'Value', '1')
+%     % Controller feedback: done variables
+%     set_param('MARCH_v1_5/State Machine/doneSitting', 'Value', '0')
+%     set_param('MARCH_v1_5/State Machine/doneStanding', 'Value', '0')
+%     set_param('MARCH_v1_5/State Machine/doneStepConst', 'Value', '0')
+%     set_param('MARCH_v1_5/State Machine/doneStepPeak', 'Value', '0')
+% end
 
 
 % Disable blocks for target simulation
