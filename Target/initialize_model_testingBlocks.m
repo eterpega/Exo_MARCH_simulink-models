a674e16d8275e149f1b1a423882e9bd9ff817e7e
Ortix <nicktsutsunava@gmail.com>
2016-10-04 16:17:30 +0200

    input device xml

--- Target/initialize_model_testingBlocks.m
+++ Target/initialize_model_testingBlocks.m
@@ -2,16 +2,16 @@
     
 
 % Input device: Trigger buttons
-set_param('MARCH_v1_5/Input Device/Right Trigger', 'Value', '0')
-set_param('MARCH_v1_5/Input Device/Left Trigger', 'Value', '0')
-set_param('MARCH_v1_5/Input Device/Both Triggers', 'Value', '0')
+set_param('MARCH_demo/Input Device/Right Trigger', 'Value', '0')
+set_param('MARCH_demo/Input Device/Left Trigger', 'Value', '0')
+set_param('MARCH_demo/Input Device/Both Triggers', 'Value', '0')
 
 % Input device: Safety buttons
-set_param('MARCH_v1_5/Input Device/Right Safety', 'Value', '0')
-set_param('MARCH_v1_5/Input Device/Left Safety', 'Value', '0')
-set_param('MARCH_v1_5/Input Device/Both Safety', 'Value', '0')
+set_param('MARCH_demo/Input Device/Right Safety', 'Value', '0')
+set_param('MARCH_demo/Input Device/Left Safety', 'Value', '0')
+set_param('MARCH_demo/Input Device/Both Safety', 'Value', '0')
 
 % Input device: scroll wheel
-set_param('MARCH_v1_5/Input Device/Right Scroll Click', 'Value', '0')
-set_param('MARCH_v1_5/Input Device/Right Scroll Mode', 'Value', '0')
+set_param('MARCH_demo/Input Device/Right Scroll Click', 'Value', '0')
+set_param('MARCH_demo/Input Device/Right Scroll Mode', 'Value', '0')
 
