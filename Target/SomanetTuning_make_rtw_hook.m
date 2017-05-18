aaa00182256b19cff98214a2a688cb5c7cca8135
march <tech@projectmarch.nl>
2017-05-18 16:02:50 +0200

    got rit of unnecesery xml files. Big problem still: Position control while NOT in tuning mode

--- Target/SomanetTuning_make_rtw_hook.m
+++ Target/SomanetTuning_make_rtw_hook.m
@@ -4,14 +4,14 @@
 % should not have to be repeated, but for future reference
 
 disp(['MARCH S&C: Resetting slider constants']);
-setConstant('position_p_gui'        , 200000);
+setConstant('position_p_gui'        , 20000);
 setConstant('position_i_gui'        , 0);
-setConstant('position_d_gui'        , 900000);
+setConstant('position_d_gui'        , 90000);
 setConstant('velocity_p_gui'        , 700000);
 setConstant('velocity_i_gui'        , 20000);
 setConstant('velocity_d_gui'        , 0);
 setConstant('min_position_slider'   , -500000000);
 setConstant('max_position_slider'   , 500000000);
 setConstant('max_torque_slider'     , 2122);
-setConstant('angle_offset_slider'   , 2858);
+setConstant('angle_offset_slider'   , 900);
 setConstant('changemeplease'        , 72);
