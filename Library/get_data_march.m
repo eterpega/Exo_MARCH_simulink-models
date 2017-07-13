d3276e7fac1657b383a2582e5878d822f84c416b
nathantim <timmerhout@gmail.com>
2017-07-13 18:02:44 +0200

    Working an data retreiving function: get_data_march.m. Almost finished. Only saving the data does not work properly yet

--- Library/get_data_march.m
+++ Library/get_data_march.m
@@ -8,7 +8,34 @@ function get_data_march(name)
 %Then here add the code to open and plot the data.
 
 % Attach to the target computer file system.
-close all
-f=SimulinkRealTime.fileSystem;
+close all;clear;clc;
+% f=SimulinkRealTime.fileSystem;
+% 
+% read = fopen(f,name);
+% rawData = fread(f,read);
+% f.fclose(read);
+
+
+dataStruct = SimulinkRealTime.utils.getFileScopeData(rawData);
+numSignal = dataStruct.numSignals;
+sizeArray = size(dataStruct.data);
+length = sizeArray(1);
+nameSignal = cell(numSignal,1);
+data = zeros(length,1);
+for j = 1:numSignal
+    nameSignal(j) = regexprep(reverse(strtok(reverse(dataStruct.signalNames(j)),'/')),' ','');
+end
+time = dataStruct.data(:,numSignal);
+
+for i = 1:(numSignal-1)
+    data = dataStruct.data(:,i);
+    figure(i)
+    plot(time,data);
+    title(['Figure ',int2str(i)]);
+    xlabel(nameSignal(numSignal))
+    ylabel(nameSignal(i))
+    save(char('Data_files/',strcat(strtok(name,'.'),nameSignal(i))),data)
+end
+
 
 end
