f5d4aaf43a9b40eaa672e9efd9337bd6c5e383df
march <tech@projectmarch.nl>
2017-07-21 16:07:52 +0200

    merge

--- Library/Enums/StepType.m
+++ Library/Enums/StepType.m
@@ -1,8 +1,9 @@
 classdef StepType < Simulink.IntEnumType
     enumeration
         FULLSTEP(1)
-        HALFSTEP(2)
-        STAIRSTEP(3)
-        NOSTEP(4)
+        HALFSTEPSTART(2)
+        HALFSTEPSTOP(3)
+        STAIRSTEP(4)
+        NOSTEP(5)
     end 
 end
