880b7c06ece8c3021d63026e6da13aeb1106db8c
l2late <lucadelaat@gmail.com>
2017-08-02 19:49:48 +0200

    small changes to custom lookup table

--- Library/GaitHandler/lookup_table.m
+++ Library/GaitHandler/lookup_table.m
@@ -1,9 +1,11 @@
 function [angle] = lookup_table(gaitVector, phase)
-
-if length(gaitVector)<phase
-    error('Phase is out of bounds');
-    return
-elseif length(gaitVector)>=phase
-    angle=gaitVector(phase,1);
+angle=666;
+if phase < 0 %Phase should never get negative
+    error('Phase is negative');
+else
+    if length(gaitVector) < phase
+        error('Phase is out of bounds');
+    elseif length(gaitVector) >= phase
+        angle=gaitVector(phase);
+    end
 end
-
