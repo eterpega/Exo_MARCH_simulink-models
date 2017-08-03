33a6c58e3b8f5e880ef0a8329ac99bea3640f8b2
nathantim <timmerhout@gmail.com>
2017-08-03 15:28:29 +0200

    Merge

--- Tests/StateMachineAndGaitPlayback/plot_joint.m
+++ Tests/StateMachineAndGaitPlayback/plot_joint.m
@@ -1,4 +1,4 @@
-function plot_joint(actualAngleJoint, desiredAngleJoint, masterState, desiredState, stepType, time, plotTitle)
+function plot_joint(actualAngleJoint, masterState, desiredState, stepType, time, plotTitle)
 
 yLabelsStates ={'HOLDSIT(1)', 'MANUAL(2)', 'STANDINGUP(3)', 'HOLDSTAND(4)',...
         'SITTINGDOWN(5)', 'CONTINUOUSGAIT(6)', 'STAIRS(7)', 'SLOPE(8)',...
@@ -14,56 +14,47 @@ end
 
 %% Angles
 figure('Visible','On','Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-plot(time, desiredAngleJoint);
-hold on
+subplot(2,1,1)
+title(plotTitle)
 plot(time, actualAngleJoint);
-legend('Desired angle','Actual angle')
+legend('Actual angle')
 title(plotTitle)
 xlabel('time [ms]')
 ylabel('angle [rad]')
 grid on
-saveas(gcf,['figures/', plotTitle, 'Angles'],'fig')
-close all
 
 %% States
-figure('Visible','On','Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+subplot(2,1,2)
 plot(time, masterState);
 hold on
 plot(time, desiredState);
 set(gca, 'Ytick',[1,2,3,4,5,6,7,8,9],'YTickLabel', yLabelsStates);
 axis([0 length(desiredState)/500 1 9]);
-title(plotTitle)
 xlabel('time [ms]')
 ylabel('state')
 legend('masterState','desiredState')
 grid on
-saveas(gcf,['figures/',plotTitle, 'States'],'fig')
+saveas(gcf,['figures/',plotTitle, ' And States'],'fig')
 close all
 
-%% Step Types
+%% Angles
 figure('Visible','On','Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+subplot(2,1,1)
+title(plotTitle)
+plot(time, actualAngleJoint);
+legend('Actual angle')
+title(plotTitle)
+xlabel('time [ms]')
+ylabel('angle [rad]')
+grid on
+
+%% Step Types
+subplot(2,1,2)
 plot(time, stepType);
 set(gca, 'Ytick',[1,2,3,4,5,6,7,8,9],'YTickLabel', yLabelsStepTypes);
 axis([0 length(desiredState)/500 1 9]);
-title(plotTitle)
 xlabel('time [ms]')
 ylabel('step type')
 grid on
-saveas(gcf,['figures/', plotTitle, 'StepType'],'fig')
-close all
-
-%% StatesandAngles
-figure('Visible','On','Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-plot(time, masterState);
-axis([0 max(time) 1 9]);
-set(gca, 'Ytick',[1,2,3,4,5,6,7,8,9],'YTickLabel', yLabelsStates);
-ylabel('angle [rad]')
-hold on
-yyaxis right
-plot(time, actualAngleJoint);
-ylabel('angle [rad]')
-xlabel('time [ms]')
-title(plotTitle)
-grid on
-saveas(gcf,['figures/', plotTitle, 'StatesAndAngles'],'fig')
+saveas(gcf,['figures/', plotTitle, ' And Step Types'],'fig')
 close all
