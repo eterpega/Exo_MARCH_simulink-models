736317fd55348690f3d49a69cfc130f495defdd5
nathantim <timmerhout@gmail.com>
2017-08-03 15:27:48 +0200

    Working on PDB errors and getErrorCode function

--- Library/PDBHandler/getPDBErrorFromErrorCode.m
+++ Library/PDBHandler/getPDBErrorFromErrorCode.m
@@ -1,4 +1,11 @@
 function [pdbError]  = getPDBErrorFromErrorCode(errorCode)
-
+    case hex2dec('')
+        pdbError = PDBError.PDB_TEMP_SENSOR_ERROR;
+    case 0 % then ethercat isnt working yet
+        pdbError = PDBError.PDB_DISCONNECTED;
+    case 72 % this means no error
+        pdbError = PDBError.NO_ERROR_PDB;
+    otherwise
+        pdbError = PDBError.UNKNOWN_ERROR;
 
 end
