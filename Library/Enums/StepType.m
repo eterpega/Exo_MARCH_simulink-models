892a62b6b1143ab47ffaf01712e9bf0550118473
l2late <lucadelaat@gmail.com>
2017-08-08 12:13:55 +0200

    changes to state machine to allow for stairsUp/Down

--- Library/Enums/StepType.m
+++ Library/Enums/StepType.m
@@ -5,11 +5,12 @@ classdef StepType < Simulink.IntEnumType
         FULLSTEP(1)
         HALFSTEPSTART(2)
         HALFSTEPSTOP(3)
-        STAIRSTEP(4)
+        STAIRSUP(4)
         STANDUP(5)
         SITDOWN(6)
         HOMESTAND(7)
         HOMESIT(8)
         NOSTEP(9)
+        STAIRSDOWN(10)
     end 
 end
