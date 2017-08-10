5fec7532ba024d7c5cec2248d20e2870a67d8c2e
taeke <t.deHaan-1@student.tudelft.nl>
2017-08-10 12:41:50 +0200

    Changed state machine by adding the different standup states which is required for bleeper. Also added two more master states to the enum and fixed the gait tester with the new changes.

--- Tests/StateMachineAndGaitPlayback/plot_joint.m
+++ Tests/StateMachineAndGaitPlayback/plot_joint.m
@@ -1,11 +1,12 @@
 function plot_joint(actualAngleJoint, masterState, desiredState, stepType, time, plotTitle)
-
-yLabelsStates ={'HOLDSIT(1)', 'MANUAL(2)', 'STANDINGUP(3)', 'HOLDSTAND(4)',...
+%MasterStates
+yLabelsMasterStates ={'HOLDSIT(1)', 'MANUAL(2)', 'STANDINGUP(3)', 'HOLDSTAND(4)',...
         'SITTINGDOWN(5)', 'CONTINUOUSGAIT(6)', 'STAIRS(7)', 'SLOPE(8)',...
-        'STONES(9)', 'STAIRSDOWN(10)','SEVERE_ERROR(666)'};
+        'STONES(9)', 'STAIRSDOWN(10)','INITIALIZINGSTANDUP(11)',...
+        'WAITINGSTANDUP(12)','SEVERE_ERROR(666)'};
     
     
-    
+%Step types
 yLabelsStepTypes ={'FULLSTEP(1)', 'HALFSTEPSTART(2)', 'HALFSTEPSTOP(3)', ...
 'STAIRSUP(4)','STANDUP(5)','SITDOWN(6)','HOMESTAND(7)','HOMESIT(8)',...
 'NOSTEP(9)', 'STAIRSDOWN(10)','INITIALIZESTANDUP(11)','WAITSTANDUP(12)'};
@@ -31,8 +32,8 @@ subplot(2,1,2)
 plot(time, masterState);
 hold on
 plot(time, desiredState);
-set(gca, 'Ytick',[1,2,3,4,5,6,7,8,9,10],'YTickLabel', yLabelsStates);
-axis([0 length(desiredState)/500 1 10]);
+set(gca, 'Ytick',[1,2,3,4,5,6,7,8,9,10, 11, 12],'YTickLabel', yLabelsMasterStates);
+axis([0 length(desiredState)/500 1 12]);
 xlabel('time [ms]')
 ylabel('state')
 legend('masterState','desiredState')
@@ -55,7 +56,7 @@ grid on
 subplot(2,1,2)
 plot(time, stepType);
 set(gca, 'Ytick',[1,2,3,4,5,6,7,8,9, 10, 11, 12],'YTickLabel', yLabelsStepTypes);
-axis([0 length(desiredState)/500 1 10]);
+axis([0 length(desiredState)/500 1 12]);
 xlabel('time [ms]')
 ylabel('step type')
 grid on
