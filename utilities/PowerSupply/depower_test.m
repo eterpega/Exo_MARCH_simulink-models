0f98a93bca622764c66994144a84d7ca66e726ea
l2late <lucadelaat@gmail.com>
2017-08-13 10:32:17 +0200

    added powersupply loggin finction to utilities

---
+++ utilities/PowerSupply/depower_test.m
@@ -0,0 +1,27 @@
+clear all
+close all
+clc
+
+%get data from file
+fid=fopen('Training_Ruben_1_standUp.txt');
+fmt=['%{HH:mm:ss:SSS}D %8.4f %8.4f %8.2f'];
+data=textscan(fid,fmt,'HeaderLines',1,'Delimiter',',');
+%data{5}=data{2}*.data{3};
+
+%plot data
+figure
+subplot(4,1,1)
+plot(data{1},data{2})
+title('Current (A)')
+%plot voltage
+subplot(4,1,2)
+plot(data{1},data{3})
+title('Voltage (V)')
+%plot power
+subplot(4,1,3)
+plot(data{1},data{4})
+title('PS Power (W)')
+%plot computed power
+% subplot(4,1,4)
+% plot(data{1},data{5})
+% title('Computed Power (W)')
