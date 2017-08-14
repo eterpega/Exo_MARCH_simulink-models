dabd6728141d01da169f0ebab79358b685072124
nathantim <timmerhout@gmail.com>
2017-08-14 17:50:54 +0200

    Added severe_error to buzzer enum, is also implemented in GES code

--- Library/Enums/BuzzerCommand.m
+++ Library/Enums/BuzzerCommand.m
@@ -1,8 +1,9 @@
 classdef BuzzerCommand < Simulink.IntEnumType    
     enumeration
-        NOTHING(0)
+        NOTHING(72)
         STOPTIMER(66)
         ONEBEEP(77)
-        ERROR(99) %Number 99 is comprised of the vibrations of number 9 appearing twice, amplifying its influences and energies. Number 99 relates to the attributes and energies of communication, inner-strength, leadership and leading by positive example, selflessness, and mhuch more
+        ERROR(88)
+        SEVERE_ERROR(99) %Number 99 is comprised of the vibrations of number 9 appearing twice, amplifying its influences and energies. Number 99 relates to the attributes and energies of communication, inner-strength, leadership and leading by positive example, selflessness, and mhuch more
     end 
 end
