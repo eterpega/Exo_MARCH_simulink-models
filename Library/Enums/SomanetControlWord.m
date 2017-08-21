71c116afddf7983b2c7ff41465acaacb9177c040
nathantim <timmerhout@gmail.com>
2017-08-21 16:14:19 +0200

    Added graceful opmode switch procedure in somanet statemachine

--- Library/Enums/SomanetControlWord.m
+++ Library/Enums/SomanetControlWord.m
@@ -1,5 +1,6 @@
 classdef SomanetControlWord < Simulink.IntEnumType  
     enumeration % these correspond with the actual numbers, enum can be directly converted
+        DISABLE_SWITCH_ON(4)
         SHUTDOWN(6)
         SWITCH_ON(7)
         QUICK_STOP(2)
